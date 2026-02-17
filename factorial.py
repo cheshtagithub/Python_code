7663ff09250caf9c1b44e7286464771fdbbb9afa
Cheshta Verma <ashtcheshta03@gmail.com>
2026-02-17 14:24:07 +0530

    Adding code for factorial

---
+++ factorial.py
@@ -0,0 +1,8 @@
+n = int(input("Enter the number for factorial: "))
+if (n == 0) or (n == 1):
+     print(f"Factorial of {n} is: ",1)
+
+fact = 1
+for i in range(1, n + 1):
+    fact = fact * i
+print(f"Factorial of {n} is:", fact)
