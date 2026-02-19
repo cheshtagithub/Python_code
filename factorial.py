fda3dcc78e194b0c62a01044af9b0a62dd185d44
Cheshta Verma <ashtcheshta03@gmail.com>
2026-02-19 11:41:52 +0530

    Error handling code is added to factorial code

--- factorial.py
+++ factorial.py
@@ -1,8 +1,11 @@
 n = int(input("Enter the number for factorial: "))
-if (n == 0) or (n == 1):
-     print(f"Factorial of {n} is: ",1)
+try:
+    if (n == 0) or (n == 1):
+        print(f"Factorial of {n} is: ",1)
 
-fact = 1
-for i in range(1, n + 1):
-    fact = fact * i
-print(f"Factorial of {n} is:", fact)
+    fact = 1
+    for i in range(1, n + 1):
+        fact = fact * i
+    print(f"Factorial of {n} is:", fact)
+except ValueError as e:
+    print("invalid input:", e)
